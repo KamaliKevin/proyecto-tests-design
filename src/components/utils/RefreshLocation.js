bfe33421cddea081ce81102bacf4265b21402eb6
KevinBGS <geisenhofkevin@gmail.com>
2024-02-24 14:12:02 +0000

    [CORRECTION #1] add: RefreshLocation utility component added so it refreshes the content of a page when navigating to it

--- src/components/utils/RefreshLocation.js
+++ src/components/utils/RefreshLocation.js
@@ -1,11 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const RefreshLocation = () => {
     const { pathname } = useLocation();
+    const prevPathname = useRef();
 
     useEffect(() => {
-        window.location.reload();
+        if (pathname !== prevPathname.current) {
+            window.location.reload();
+            prevPathname.current = pathname; // Actualizar la ruta anterior
+        }
     }, [pathname]);
 
     return null; // "RefreshLocation" es un componente de utilidad, por lo que no tiene que renderizar nada
